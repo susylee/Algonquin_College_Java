f5cde105b79f323d3287e9c0644e16928f6c027a
Suyeon Lee <lsy76@10.70.208.31>
2022-08-23 12:38:45 -0400

    Exercise01

---
+++ Exercise01/src/Exercise01.java
@@ -0,0 +1,26 @@
+/*Student Name : Su Yeoun Lee
+ * Lab Professor : Professor Fedor Ilitchev
+ * Due date : May 27, 2022
+ * Modified : May 16, 2022
+ * Description: This program will output some ascii art
+*/
+public class Exercise01 { //first
+	public static void main(String[] args) {
+		System.out.println("    ~~~~~~~");
+		System.out.println("  ~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~    ~~~~~");
+		System.out.println("  ||     ||    ~~~");
+		System.out.println("   ||   ||    ~~~~~");
+		System.out.println("     || |     ||");
+		System.out.println("      | |====||");
+		System.out.println("      | |");
+		System.out.println("      |O|");
+		System.out.println("      | |");
+		System.out.println("      | |");
+		System.out.println("     || ||");
+		System.out.println("****************");
+		System.out.println("****************");
+		System.out.println("Program by Su Yeoun Lee");
+	}
+}
