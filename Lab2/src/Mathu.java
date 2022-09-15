e64c4d95f22d495579a2f74edbdc1f2db528c772
Suyeon Lee <lsy7674@gmail.com>
2022-09-15 16:18:31 -0400

    Java_Level_2

---
+++ Lab2/src/Mathu.java
@@ -0,0 +1,75 @@
+/**
+ * TASK: Remember to include all necessary headers for your Javadoc, descriptions and explanations of what you have done in the appropriate places.
+ * ENSURE that you add appropriate Javadoc style comments to relevant parts of this code as you update this program to show that you understand it.
+ *
+ * @author - various - put your own name here
+ * 
+ */
+
+public class Mathu
+{
+	public static void main(String[] args)
+	{
+		final int ROWS = 7;
+		final int COLUMNS = 8;
+
+		int[][] patients = 
+			{ 
+					{  2200, 1100, 1200, 1000, 1015, 2000, 1092, 2204 },
+					{  5020, 6105, 2009, 9047, 1016, 2014, 2708, 2308 }, 
+					{  1720, 2406, 3054, 1018, 1023, 3100, 1406, 1502 }, 
+					{  1490, 2002, 2016, 5008, 2044, 1055, 1607, 2201 },
+					{  1520, 1007, 1092, 2065, 1023, 1010, 1046, 1502 },
+					{  1670, 1201, 2008, 2001, 1086, 1009, 1041, 1706 },
+					{  1870, 2001, 2078, 1006, 1053, 1702, 1009, 1406 }
+
+			};
+
+		String[] provinces = 
+			{ 
+					"Ontario", 
+					"Quebec", 
+					"Nova Scotia",
+					"New Brunswick", 
+					"Manitoba", 
+					"British Columbia",
+					"Prince Edward Island"         
+			};
+
+		//System.out.printf("%-25s %-4s %-4s %-4s %-4s %-4s %-4s %-4s %-4s ","Month","Feb","March   April     May    June    July    Aug     Sept");
+		System.out.println();
+
+		for(int i=0;i<ROWS;i++) {
+			System.out.printf("%-25s", provinces [i]+"");
+			for(int j=0;j<COLUMNS;j++) {
+				System.out.printf(patients[i][j]+"\t");    		
+			}
+			System.out.println(" ");
+		}
+
+		/** TASK: It is important to know the number of spice traders per city. So you need to print out the number of traders for all cities (content of the array) for each month.
+		 * Update the code in this section by using a nested for loop. 
+		 * 
+		 */
+
+		// TO DO: WRITE YOUR CODE FOR THIS SECTION HERE!!!! Check the sample output file (SpiceOutput) to see the expected pattern to print out these details using printf.
+
+
+
+		/** TASK: Update the code in this section by writing a nested for loop (for the columns and rows of the array) to compute the sum (column). 
+		 * Print the column sum using printf. Check the sample output file (SpiceOutput) to see the expected pattern.
+		 */ 
+
+		System.out.println();
+		System.out.printf("%-25s","Recovered Patients");
+		int[] theNumberOfMonths = new int[patients[0].length];
+		int sum = 0;
+		for(int i=0;i<theNumberOfMonths.length;i++) {
+			for(int j=0;j<ROWS;j++) {
+				sum += patients[j][i];
+			} theNumberOfMonths[i] = sum;
+			System.out.print(theNumberOfMonths[i]+"\t");
+			sum = 0;
+		}
+	}
+}
